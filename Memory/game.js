a71356de4a67c9e6f8fccaa8c3d3373d519034bd
saxatachi <saxatachi@gmail.com>
2018-11-01 15:04:00 +0100

    2 klikniecia

--- Memory/game.js
+++ Memory/game.js
@@ -5,6 +5,31 @@ const cardsColor = ["red", "red", "green", "green"
 
 let cards = document.querySelectorAll("div");
 cards = [...cards];
+const startTime = new Date().getTime();
+let activeCard = "";
+const activeCards = [];
+
+const gamePairs = cards.length / 2;
+let gameResult = 0;
+
+
+const clickCard = function ()
+{
+    activeCard = this;
+    activeCard.classList.remove("hidden");
+
+    if (activeCards.length === 0) {
+        activeCards[0] = activeCard;
+        return;
+    } else
+    {
+        cards.forEach(card =>
+        {
+            card.removeEventListener("click", clickCard);
+        })
+    }
+
+};
 const init = function ()
 {
     cards.forEach(function (card)
@@ -13,5 +38,12 @@ const init = function ()
         card.classList.add(cardsColor[position]);
         cardsColor.splice(position,1);
     })
+    setTimeout(function () {
+        cards.forEach(function (card)
+        {
+            card.classList.add("hidden");
+            card.addEventListener("click",clickCard)
+        })
+    }, 2000)
 }
 init()
