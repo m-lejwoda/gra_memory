78f785a861cad41c9b2ba7ed8b54eddce30b3fdd
saxatachi <saxatachi@gmail.com>
2018-11-01 15:27:30 +0100

    prawie gotowe

--- Memory/game.js
+++ Memory/game.js
@@ -27,6 +27,28 @@ const clickCard = function ()
         {
             card.removeEventListener("click", clickCard);
         })
+        activeCards[1] = activeCard;
+        setTimeout(function () {
+            if (activeCards[0].className === activeCards[1].className) {
+                activeCards.forEach(card => card.classList.add("off"))
+                gameResult++;
+                if (gameResult == gamePairs)
+                {
+                    const endTime = new Date().getTime();
+                    constgameTime = (endTime - startTime) / 1000;
+                    alert(`Uda³o siê Czas to ${gameTime} sekund`);
+                    location.reload();
+                }
+
+            } else {
+                activeCards.forEach(card => card.classList.add("hidden"))
+            }
+            activeCard = "";
+            activeCards.length = 0;
+            cards.forEach(card => card.addEventListener("click", clickCard))
+
+        }, 500)
+        
     }
 
 };
