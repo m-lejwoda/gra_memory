61efb69fbb043017fd2075c3cc62504ee83e3806
saxatachi <saxatachi@gmail.com>
2018-11-01 14:30:27 +0100

    losowanie

--- Memory/game.js
+++ Memory/game.js
@@ -1,1 +1,14 @@
-// JavaScript source code
+const cardsColor = ["red", "red", "green", "green"
+    , "blue", "blue", "brown", "brown", "yellow", "yellow",
+    "gray", "gray", "cadetblue","cadetblue", "violet", "violet",
+    "lightgreen", "lightgreen"];
+
+let cards = document.querySelectorAll("div");
+cards = [...cards];
+const init = function ()
+{
+    cards.forEach(function (card)
+    {
+        const position = Math.floor(Math.random() * cardsColor.length);
+    })
+}
